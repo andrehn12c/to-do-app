50ac17549d7aebdc519c5acfb0c1aa721bebc744
André Henrique de Almeida Nascimento <andrehenriquenascimento1@gmail.com>
2023-11-27 21:29:22 -0300

    iniciando conexão com o MySQL

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const exphbs = require('express-handlebars')
 
+const mysql = require("mysql2")
+
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
@@ -13,6 +15,10 @@ app.get(`/`, (requisicao, resposta) => {
     resposta.render('home')
 })
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "todoapp"
 })
+
